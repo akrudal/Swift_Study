ecd0114af66fc13e72fbe0b9fd61d9bd82e7b6e4
akrudal <magyeongmi@magyeongmiui-MacBookPro.local>
2022-04-29 21:09:34 +0900

    [ADD] mapkit 디자인 수정

--- mapkit/mapkitTest/mapkitTest/ViewController.swift
+++ mapkit/mapkitTest/mapkitTest/ViewController.swift
@@ -16,11 +16,41 @@ class ViewController: UIViewController {
     @IBOutlet var remainTimeLabel: UILabel!
     @IBOutlet var myImage: UIImageView!
     @IBOutlet weak var mapView: MapKitView!
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-      mapView.configure(with: UIColor.green)
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    
+    @IBOutlet var liveView: UIView!
+    @IBOutlet var liveButton: UIButton!
+    
+    @IBOutlet var heightConstraint: NSLayoutConstraint!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mapView.configure(with: UIColor.green)
+        setShadow()
   }
+    func setShadow() {
+        button1.layer.shadowOpacity = 0.25
+        button1.layer.shadowOffset = CGSize(width: 0, height: 1)
+        button1.layer.shadowRadius = 4
+        button1.layer.masksToBounds = false
+
+        button2.layer.shadowOpacity = 0.25
+        button2.layer.shadowOffset = CGSize(width: 0, height: 1)
+        button2.layer.shadowRadius = 4
+        button2.layer.masksToBounds = false
+    }
   
+    @IBAction func clickLive(_ sender: Any) {
+        UIView.animate(withDuration: 0.5,
+                       delay: 0,
+                       options: .curveEaseIn,
+                       animations: { () -> Void in
+            self.heightConstraint.constant = 170
+            self.view.layoutIfNeeded()
+        },
+                       completion: { (didFinish) -> Void in
+        })
+    }
 }
 
