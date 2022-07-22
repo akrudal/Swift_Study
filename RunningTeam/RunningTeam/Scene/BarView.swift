0d5a17616993e8bd790a9ef5d869351cb3b78e13
akrudal <magyeongmi@Rapunzel.local>
2022-07-22 22:05:04 +0900

    [ADD] RunningTeam Init View

---
+++ RunningTeam/RunningTeam/Scene/BarView.swift
@@ -0,0 +1,37 @@
+//
+//  BarView.swift
+//  RunningTeam
+//
+//  Created by 마경미 on 2022/07/22.
+//
+
+import UIKit
+
+@IBDesignable
+class BarView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        loadView()
+    }
+    
+    private func loadView() {
+        guard let view = loadViewFromNib(nib: "BarView") else {
+            return
+        }
+        view.frame = bounds
+        view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        addSubview(view)
+    }
+
+    func loadViewFromNib(nib: String) -> UIView? {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: nib, bundle: bundle)
+        return nib.instantiate(withOwner: self, options: nil).first as? UIView
+    }
+}
---
+++ RunningTeam/RunningTeam/Scene/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  RunningTeam
+//
+//  Created by 마경미 on 2022/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor(red: 0.898, green: 0.898, blue: 0.898, alpha: 1)
+    }
+}
+
